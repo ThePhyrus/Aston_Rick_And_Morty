86b3969ba0ad9b63c2a5112dfc56b2a5f842d301
ThePhyrus <romaisgood@yandex.ru>
2022-12-04 18:33:04 +0300

    Набросал фрагменты и навигацию (сероп)

--- app/src/main/java/roman/bannikov/aston_rick_and_morty/listeners/OnCharacterCardClickListener.kt
+++
@@ -1,8 +0,0 @@
-package roman.bannikov.aston_rick_and_morty.listeners
-
-import roman.bannikov.aston_rick_and_morty.models.CharacterModel
-
-interface OnCharacterCardClickListener {
-    //todo сделать функциональным, чтобы использовать ля-ля-лямбду
-    fun launchCharacterDetailsFragment(character: CharacterModel)
-}
---
+++ app/src/main/java/roman/bannikov/aston_rick_and_morty/view/fragments/episodes/EpisodesFilterFragment.kt
@@ -0,0 +1,66 @@
+package roman.bannikov.aston_rick_and_morty.view.fragments.episodes
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import roman.bannikov.aston_rick_and_morty.databinding.FragmentEpisodesFilterBinding
+import roman.bannikov.aston_rick_and_morty.view.navigator
+
+class EpisodesFilterFragment : BottomSheetDialogFragment() {
+
+    private lateinit var binding: FragmentEpisodesFilterBinding
+    private var episode: List<String>? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentEpisodesFilterBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnApplyFilterEpisodes.setOnClickListener {
+            navigator().openEpisodesFragmentWithArg(episode = null)
+            dismiss()
+        }
+        binding.btnFilterEpisodesEpisodes.setOnClickListener {
+            episode = getOtherParams(listOf<String>("type", "2", "3", "4"), "Episodes")
+        }
+    }
+
+    private fun getOtherParams(params: List<String>, paramsType: String): List<String>? {
+
+        val typesArr = params.toTypedArray()
+        val checkedItems: BooleanArray = typesArr.map { false }.toBooleanArray()
+
+        val result = ArrayList<String>()
+
+        val dialog = AlertDialog.Builder(requireContext())
+            .setTitle(paramsType)
+            .setMultiChoiceItems(
+                typesArr,
+                checkedItems
+            ) { _, which, isChecked ->
+                if (isChecked) {
+                    result.add(typesArr[which])
+                }
+            }
+            .setPositiveButton(
+                "Confirm"
+            ) { _, _ ->
+                Toast.makeText(requireContext(), "$result", Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton("Cancel", null)
+            .create()
+        dialog.show()
+        return result
+    }
+}
