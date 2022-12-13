cc99266d22e6b5968a7cfe913d3878102feb9eaa
ThePhyrus <romaisgood@yandex.ru>
2022-12-14 01:38:40 +0300

    Подходит...

--- app/src/main/java/roman/bannikov/aston_rick_and_morty/data/storage/sharedPref/EpisodeSettingsPref.kt
+++ app/src/main/java/roman/bannikov/aston_rick_and_morty/data/storage/sharedPref/EpisodeSettingsPref.kt
@@ -2,24 +2,26 @@ package roman.bannikov.aston_rick_and_morty.data.storage.sharedPref
 
 import android.content.Context
 
-private const val SHARED_PREFS_NAME = "shared_prefs_name"
+private const val SHARED_PREF_NAME = "shared_pref_name"
+private const val KEY_EPISODES = "episodes"
 private const val EPISODES = "EPISODES"
+private const val DEFAULT_VALUE = "S01E01"
 
 class EpisodeSettingsPref(context: Context) : SharedPrefStorage {
 
     private val sharedPreferences =
-        context.getSharedPreferences(SHARED_PREFS_NAME, Context.MODE_PRIVATE)
+        context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
 
     override suspend fun save(episodes: Map<String, List<String>>) {
-        val episodes = episodes.getValue("episodes").joinToString(separator = "\n")
+        val episodes = episodes.getValue(KEY_EPISODES).joinToString(separator = "\n")
         sharedPreferences.edit().putString(EPISODES, episodes).apply()
     }
 
     override suspend fun get(): Map<String, List<String>> {
         val episodes: List<String> =
-            (sharedPreferences.getString(EPISODES, "S01E01") ?: "S01E01").lines()
+            (sharedPreferences.getString(EPISODES, DEFAULT_VALUE) ?: DEFAULT_VALUE).lines()
         return mapOf(
-            "episodes" to episodes
+            KEY_EPISODES to episodes
         )
     }
 }
