0ed7f1397a0c69ab3a346ae07aa6401fc1d0a5ea
ThePhyrus <romaisgood@yandex.ru>
2022-12-16 20:16:14 +0300

    Исправил CharacterFilterFragment.kt

--- app/src/main/java/roman/bannikov/aston_rick_and_morty/view/fragments/character/CharacterFilterFragment.kt
+++ app/src/main/java/roman/bannikov/aston_rick_and_morty/view/fragments/character/CharacterFilterFragment.kt
@@ -2,7 +2,6 @@ package roman.bannikov.aston_rick_and_morty.view.fragments.character
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,6 +13,7 @@ import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.chip.Chip
 import kotlinx.coroutines.launch
+import roman.bannikov.aston_rick_and_morty.R
 import roman.bannikov.aston_rick_and_morty.databinding.FragmentCharacterFilterBinding
 import roman.bannikov.aston_rick_and_morty.utils.navigator
 import roman.bannikov.aston_rick_and_morty.viewmodel.character.CharacterFilterViewModel
@@ -22,33 +22,115 @@ import roman.bannikov.aston_rick_and_morty.viewmodel.character.CharacterFilterVi
 
 class CharacterFilterFragment : BottomSheetDialogFragment() {
 
-    private lateinit var binding: FragmentCharacterFilterBinding
+    private var _binding: FragmentCharacterFilterBinding? = null
+    private val binding: FragmentCharacterFilterBinding get() = _binding!!
+
+    private lateinit var viewModel: CharacterFilterViewModel
+
     private var species: String = ""
-    private var type: String = ""
     private var speciesList: MutableList<String> = mutableListOf<String>()
+    private var type: String = ""
     private var typesList: MutableList<String> = mutableListOf<String>()
-    private lateinit var vm: CharacterFilterViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentCharacterFilterBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentCharacterFilterBinding.inflate(layoutInflater, container, false)
+        initButtons()
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initViewModel()
 
-        vm = ViewModelProvider(
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(
             this,
             CharacterFilterViewModelProvider(requireContext())
         )[CharacterFilterViewModel::class.java]
-        observeVm()
+        observeViewModel()
+    }
 
+    private fun filterSpecies(params: List<String>) {
+        val typedArray = params.toTypedArray()
+
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.characters_species))
+            .setSingleChoiceItems(typedArray, 0, null)
+            .setPositiveButton(getString(R.string.ok)) { dialog, _ ->
+                dialog.dismiss()
+                val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
+                if (typedArray.isNotEmpty()) {
+                    species = typedArray[selectedPosition]
+                }
+
+            }
+            .setNegativeButton(getString(R.string.cancel), null)
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun filterType(params: List<String>) {
+        val typedArray = params.toTypedArray()
+
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.character_types))
+            .setSingleChoiceItems(typedArray, 0, null)
+            .setPositiveButton(getString(R.string.ok)) { dialog, _ ->
+                dialog.dismiss()
+                val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
+                if (typedArray.isNotEmpty()) {
+                    type = typedArray[selectedPosition]
+                }
+
+            }
+            .setNegativeButton(getString(R.string.cancel), null)
+            .setCancelable(false)
+            .show()
+    }
+
+    private fun filterGender(): String {
+        binding.cgCharacterGenderFilter.children
+            .toList()
+            .filter { (it as Chip).isChecked }
+            .forEach { return (it as Chip).text.toString() }
+        return ""
+    }
+
+    private fun filterStatus(): String {
+        binding.cgCharacterStatusFilter.children
+            .toList()
+            .filter { (it as Chip).isChecked }
+            .forEach { return (it as Chip).text.toString() }
+        return ""
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.speciesList.collect {
+                    speciesList.addAll(it)
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.RESUMED) {
+                viewModel.typeList.collect {
+                    typesList.addAll(it)
+                }
+            }
+        }
+    }
+
+    private fun initButtons() {
         binding.btnApplyFilterCharacter.setOnClickListener {
-            val gender = getGender()
-            val status = getStatus()
+            val gender = filterGender()
+            val status = filterStatus()
 
             navigator().launchFilteredCharacterListFragment(
                 gender = gender,
@@ -60,80 +142,16 @@ class CharacterFilterFragment : BottomSheetDialogFragment() {
         }
 
         binding.btnFilterCharacterType.setOnClickListener {
-            getType(typesList)
+            filterType(typesList)
         }
 
         binding.btnFilterCharacterSpecies.setOnClickListener {
-            getSpecies(speciesList)
-        }
-
-    }
-
-    private fun observeVm() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                vm.speciesList.collect {
-                    speciesList.addAll(it)
-                }
-            }
-        }
-
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.RESUMED) {
-                vm.typeList.collect {
-                    typesList.addAll(it)
-                }
-            }
+            filterSpecies(speciesList)
         }
     }
 
-    private fun getStatus(): String {
-        binding.cgCharacterStatusFilter.children
-            .toList()
-            .filter { (it as Chip).isChecked }
-            .forEach { return (it as Chip).text.toString() }
-        return ""
-    }
-
-    private fun getGender(): String {
-        binding.cgCharacterGenderFilter.children
-            .toList()
-            .filter { (it as Chip).isChecked }
-            .forEach { return (it as Chip).text.toString() }
-        return ""
-    }
-
-    private fun getSpecies(params: List<String>) {
-        val typesArr = params.toTypedArray()
-
-        AlertDialog.Builder(requireContext())
-            .setTitle("Characters species")
-            .setSingleChoiceItems(typesArr, 0, null)
-            .setPositiveButton("Confirm") { dialog, _ ->
-                dialog.dismiss()
-                val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
-                Log.e("checkedItem", "$selectedPosition");
-                if(typesArr.isNotEmpty()){ species = typesArr[selectedPosition] }
-
-            }
-            .setNegativeButton("Cancel", null)
-            .show()
-    }
-
-    private fun getType(params: List<String>) {
-        val typesArr = params.toTypedArray()
-
-        AlertDialog.Builder(requireContext())
-            .setTitle("Characters types")
-            .setSingleChoiceItems(typesArr, 0, null)
-            .setPositiveButton("Confirm") { dialog, _ ->
-                dialog.dismiss()
-                val selectedPosition = (dialog as AlertDialog).listView.checkedItemPosition
-                Log.e("checkedItem", "$selectedPosition");
-                if(typesArr.isNotEmpty()){ type = typesArr[selectedPosition] }
-
-            }
-            .setNegativeButton("Cancel", null)
-            .show()
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
