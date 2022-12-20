871ff306a4f732e13c7b82b36bc2f03c432cfed6
ThePhyrus <romaisgood@yandex.ru>
2022-12-20 17:19:53 +0300

    Исправил LocationDetailsFragment.kt

--- app/src/main/java/roman/bannikov/aston_rick_and_morty/view/fragments/location/LocationDetailsFragment.kt
+++ app/src/main/java/roman/bannikov/aston_rick_and_morty/view/fragments/location/LocationDetailsFragment.kt
@@ -10,12 +10,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.coroutineScope
 import androidx.paging.ExperimentalPagingApi
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.launch
 import roman.bannikov.aston_rick_and_morty.databinding.FragmentLocationDetailsBinding
-import roman.bannikov.aston_rick_and_morty.view.models.location.LocationView
 import roman.bannikov.aston_rick_and_morty.utils.navigator
 import roman.bannikov.aston_rick_and_morty.view.adapters.character.CharacterListForDetailsAdapter
+import roman.bannikov.aston_rick_and_morty.view.models.location.LocationView
 import roman.bannikov.aston_rick_and_morty.view.viewmodels.location.LocationDetailsViewModel
 import roman.bannikov.aston_rick_and_morty.view.viewmodels.location.LocationDetailsViewModelProvider
 import kotlin.properties.Delegates
@@ -23,11 +22,83 @@ import kotlin.properties.Delegates
 @ExperimentalPagingApi
 class LocationDetailsFragment : Fragment() {
 
-    private lateinit var binding: FragmentLocationDetailsBinding
-    private lateinit var vm: LocationDetailsViewModel
+    private var _binding: FragmentLocationDetailsBinding? = null
+    private val binding: FragmentLocationDetailsBinding get() = _binding!!
+
+    private lateinit var viewModel: LocationDetailsViewModel
+
     private var characterListForDetailsAdapter: CharacterListForDetailsAdapter? = null
 
     private var locationId by Delegates.notNull<Int>()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        init()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLocationDetailsBinding.inflate(layoutInflater, container, false)
+        binding.btnBack.setOnClickListener {
+            navigator().goBack()
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViewModel()
+        initView()
+        observeViewModel()
+    }
+
+    private fun initViewModel() {
+        viewModel = ViewModelProvider(
+            this,
+            LocationDetailsViewModelProvider(requireContext())
+        )[LocationDetailsViewModel::class.java]
+        viewModel.getLocation(locationId)
+    }
+
+    private fun initView() {
+        characterListForDetailsAdapter = CharacterListForDetailsAdapter()
+        with(binding.rvLocationDetails) {
+            layoutManager = GridLayoutManager(requireContext(), 2)
+            adapter = characterListForDetailsAdapter
+        }
+        characterListForDetailsAdapter!!.onCharacterItem =
+            { navigator().launchCharacterDetailsFragment(it.id) }
+    }
+
+    private fun observeViewModel() {
+        lifecycle.coroutineScope.launch {
+            viewModel.charactersList.observe(viewLifecycleOwner, Observer {
+                characterListForDetailsAdapter!!.submitList(it)
+            })
+        }
+
+        lifecycle.coroutineScope.launch {
+            viewModel.locationDetails.observe(viewLifecycleOwner, Observer {
+                viewModel.getEpisodesList(it.residentsIds)
+                initUI(it)
+            })
+        }
+    }
+
+    private fun initUI(locationDetails: LocationView) {
+        binding.tvLocationNameInLocationDetails.text = locationDetails.name
+        binding.tvLocationTypeInLocationDetails.text = locationDetails.type
+        binding.tvLocationDimensionInLocationDetails.text = locationDetails.dimension
+    }
+
+    private fun init() {
+        arguments?.let {
+            locationId = it.getInt(KEY_LOCATION_ID)
+        }
+    }
 
     companion object {
         private const val KEY_LOCATION_ID: String = "KEY_LOCATION_ID"
@@ -43,66 +114,8 @@ class LocationDetailsFragment : Fragment() {
             }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        init()
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentLocationDetailsBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        vm = ViewModelProvider(
-            this,
-            LocationDetailsViewModelProvider(requireContext())
-        )[LocationDetailsViewModel::class.java]
-        vm.getLocation(locationId)
-        initView()
-        observeVm()
-    }
-
-    private fun initView() {
-        characterListForDetailsAdapter = CharacterListForDetailsAdapter()
-
-        with(binding.rvLocationDetails) {
-            layoutManager = LinearLayoutManager(requireContext())
-            layoutManager = GridLayoutManager(requireContext(), 2)
-            adapter = characterListForDetailsAdapter
-        }
-        characterListForDetailsAdapter!!.onCharacterItem =
-            { navigator().launchCharacterDetailsFragment(it.id) }
-    }
-
-    private fun observeVm() {
-        lifecycle.coroutineScope.launch {
-            vm.charactersList.observe(viewLifecycleOwner, Observer {
-                characterListForDetailsAdapter!!.submitList(it)
-            })
-        }
-
-        lifecycle.coroutineScope.launch {
-            vm.locationDetails.observe(viewLifecycleOwner, Observer {
-                vm.getEpisodesList(it.residentsIds)
-                initUI(it)
-            })
-        }
-    }
-
-    private fun initUI(locationDetails: LocationView) {
-        binding.tvLocationNameInLocationDetails.text = locationDetails.name
-        binding.tvLocationTypeInLocationDetails.text = "Type: ${locationDetails.type}"
-        binding.tvLocationDimensionInLocationDetails.text = "Dimension: ${locationDetails.dimension}"
-    }
-
-    private fun init() {
-        arguments?.let {
-            locationId = it.getInt(KEY_LOCATION_ID)
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
