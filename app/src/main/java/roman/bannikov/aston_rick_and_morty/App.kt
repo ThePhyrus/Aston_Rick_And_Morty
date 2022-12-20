0dd9013f31bac537c233c77f60879f268bfe8b71
ThePhyrus <romaisgood@yandex.ru>
2022-12-20 18:59:08 +0300

    Branch created commit2

--- app/src/main/java/roman/bannikov/aston_rick_and_morty/App.kt
+++ app/src/main/java/roman/bannikov/aston_rick_and_morty/App.kt
@@ -4,5 +4,7 @@ import android.app.Application
 import roman.bannikov.aston_rick_and_morty.models.CharacterModelService
 
 class App : Application() {
+
     val characterModelService = CharacterModelService()
+
 }
